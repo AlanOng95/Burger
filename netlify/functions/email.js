11409d598060d9bb3c837721bf5f752e40a2a7bb
AlanOng95 <ong80@hotmail.com>
2022-04-20 22:20:00 +0800

    emailready 2

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -16,29 +16,30 @@ exports.handler = async function (event, context) {
   emailContent = emailContent + `\n Total Amount : ${total.toFixed(2)}`;
 
   const email = {
-    from: 'ong80@hotmail.com',
+    from: "ong80@hotmail.com",
     to: customerEmail,
     subject: "New Order Received",
     text: emailContent,
   };
 
   const mailer = nodemailer.createTransport({
-      host: 'smtp.sendgrid.net',
-      port: 465,
-      secure: true,
-      auth: {
-        user: 'apikey',
-        pass: process.env.sendgridApiKey
-      });
-    try {
-      await mailer.sendMail(email);
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: "Email sent successfully",
-        }),
-      };
-    } catch (error) {
-      console.log('Error sending email', error);
+    host: 'smtp.sendgrid.net',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'apikey',
+      pass: process.env.sendgridApiKey
     }
   });
+  try {
+    await mailer.sendMail(email);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Email sent successfully",
+      }),
+    };
+  } catch (error) {
+    console.log('Error sending email', error);
+  }
+};
