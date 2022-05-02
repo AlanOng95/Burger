dbb191f8a8b201242fdee034d20079a9bec84882
AlanOng95 <ong80@hotmail.com>
2022-05-02 19:49:30 +0800

    fix

--- netlify/functions/readorders.js
+++ netlify/functions/readorders.js
@@ -9,6 +9,17 @@ const {
 const serviceAccount = JSON.parse(process.env.firebaseCredential);
 
 exports.handler = async function (event, context) {
+  const user = context.clientContext.user;
+
+  if (user.email != "alanong95@hotmail.com") {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        message: "You are not allowed to retrieve orders",
+      }),
+    };
+  }
+
   let app;
   if (admin.apps.length === 0) {
     app = initializeApp({
